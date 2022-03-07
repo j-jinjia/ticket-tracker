13ae3e1e5adc1d604bd18af3004599f5006928df
j-jinjia <j.jinjia99@gmail.com>
2022-03-07 15:20:01 +0100

    first attempt with pseudo code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 
 import './App.scss';
 
+
+/** PSEUDO CODE FOR TICKET TRACKER
+ * COMPONENTS:
+ * 1.- Showing name and role
+ * 2.- Buttons to decrease/increase ticket count
+ * 3.- Display ticket count
+ * PROPS:
+ * Imported from data.js. This object includdes and array an ID, name and role for each element. Remember to use a key for each element of the array.
+ * 
+ */
 const App = ()=> {
   return (
     <div>Working!</div>
