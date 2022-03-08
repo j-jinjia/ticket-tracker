eee2abb8e5b3e14d09742355b9265f1a76c96458
j-jinjia <j.jinjia99@gmail.com>
2022-03-08 17:02:49 +0100

    rendered all components in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-
-import './App.scss';
-import EmployeeInformation from './assets/components/Employee/Employee';
-import team from './data/data';
-
+import "./App.scss";
+import EmployeeCard from "./assets/components/Employee/Employee";
+import team from "./data/data";
+import TicketCount from "./assets/components/TicketCount/TicketCount";
 
 /** PSEUDO CODE FOR TICKET TRACKER
  * COMPONENTS:
@@ -11,17 +10,33 @@ import team from './data/data';
  * 3.- Display ticket count
  * PROPS:
  * Imported from data.js. This object includdes and array an ID, name and role for each element. Remember to use a key for each element of the array.
- * 
+ *
  */
-const App = ()=> {
+const App = () => {
+  const employee = team.map((teamEmployee) => {
+    return (
+      <>
+      <div className="team__card">
+        <EmployeeCard
+          employeeName={teamEmployee.name}
+          employeeRole={teamEmployee.role}
+        />
+        <TicketCount/>
+      </div>
+      </>
+
+    );
+  });
+
+
   return (
     <>
-      <h1 className='header'>Ticket Tracker</h1>
-      <div className="software-team">
-        <EmployeeInformation employeeArr={team}/>
+      <h1 className="header">Ticket Tracker</h1>
+      <div className="team">
+        {employee}
       </div>
     </>
   );
-}
+};
 
 export default App;
